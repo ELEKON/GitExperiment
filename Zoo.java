381129ee05f83c50eccd44e9f7aa91166227bfec
ELEKON <konelefas@hotmail.com>
2021-02-15 17:10:39 +0200

    Changes on Zoo class extras 3

--- Zoo.java
+++ Zoo.java
@@ -2,13 +2,13 @@ public class Zoo {
 
     public static void main(String[] args) {
         Animal animal1 =new Fish(5, "mame", 134);
-		Animal animal2 =new Bird(5, "female", 90);
+		Animal animal5 =new Bird(5, "female", 90);
 		Animal animal3 =new Chicken(14, "mame", 134);
 
 		animal1.eat();
 
 
-		animal2.sleep();
+		animal5.sleep();
 		animal3.sleep();
         Bird bird1 = new Bird(10, "male", 34);
         bird1.sleep();// oi methodoi tou gonea klhronomountai to antistrofo den isxuei
